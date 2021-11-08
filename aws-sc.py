424491fea64c5f7bc57bbaa8d4301095e6fe6767
Tsimur <7901614@gmail.com>
2021-11-08 22:43:02 +0300

    1-4 Fix3

--- aws-sc.py
+++ aws-sc.py
@@ -2,13 +2,13 @@ import os
 import json
 import re
 
-# #####################################
-# # 1) Output in table view ec2 instances who has tag "Environment" = "Production"
-# os.system('aws ec2 describe-instances --filters Name=tag:Environment,Values=Production --output table --region '
-#           'eu-central-1')
-
 #####################################
-# 2) Take raw json and and process it
+# 1) Output in table view ec2 instances who has tag "Environment" = "Production"
+os.system('aws ec2 describe-instances --filters Name=tag:Environment,Values=Production --output table --region '
+          'eu-central-1')
+
+####################################
+2) Take raw json and and process it
 raw_json_ec2 = os.popen('aws ec2 describe-instances --output json --region eu-central-1').read()
 py_json_ec2 = json.loads(raw_json_ec2)
 
@@ -25,13 +25,13 @@ for vm_ec2 in py_json_ec2["Reservations"]:
             print("SSH Keyname: " + vm_ec2["Instances"][0].get('KeyName'))
             print(" ")
 
-# #####################################
-# # 3)Output rds  instances so that maintenance needed
-# os.system('aws rds describe-pending-maintenance-actions --output table')
-#
-# #####################################
-# # 4) Output all the elastic Ip's that are not in use
-# os.system('aws ec2 describe-addresses --query "Addresses[?NetworkInterfaceId == null ].PublicIp"')
-#
-# #####################################
-# # 5) Output all the SSL from ACM that are not in use
+#####################################
+# 3)Output rds  instances so that maintenance needed
+os.system('aws rds describe-pending-maintenance-actions --output table')
+
+#####################################
+# 4) Output all the elastic Ip's that are not in use
+os.system('aws ec2 describe-addresses --query "Addresses[?NetworkInterfaceId == null ].PublicIp"')
+
+#####################################
+# 5) Output all the SSL from ACM that are not in use
