87730388129c1a45a5b521ba64ed5d080a7ecb3b
Tsimur <7901614@gmail.com>
2021-11-10 18:02:01 +0300

    1-5

--- aws-sc.py
+++ aws-sc.py
@@ -8,7 +8,7 @@ os.system('aws ec2 describe-instances --filters Name=tag:Environment,Values=Prod
           'eu-central-1')
 
 ####################################
-#2) Take raw json and and process it
+# 2) Take raw json and and process it
 raw_json_ec2 = os.popen('aws ec2 describe-instances --output json --region eu-central-1').read()
 py_json_ec2 = json.loads(raw_json_ec2)
 
@@ -33,5 +33,14 @@ os.system('aws rds describe-pending-maintenance-actions --output table')
 # 4) Output all the elastic Ip's that are not in use
 os.system('aws ec2 describe-addresses --query "Addresses[?NetworkInterfaceId == null ].PublicIp"')
 
-#####################################
+####################################
 # 5) Output all the SSL from ACM that are not in use
+raw_cert_list = os.popen('aws acm list-certificates').read()
+py_cert_list = json.loads(raw_cert_list)
+cert_arns = [arn_item['CertificateArn'] for arn_item in py_cert_list['CertificateSummaryList']]
+
+for arn in cert_arns:
+    raw_json_cert = os.popen('aws acm describe-certificate --certificate-arn ' + arn).read()
+    py_json_cert = json.loads(raw_json_cert)
+    if len(py_json_cert['Certificate']['InUseBy']) >= 1:
+        print(py_json_cert['Certificate']['Serial'])
